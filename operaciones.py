66b90a6c7f4fe4068e7520703f3ad69c31a14882
Facu Ur <82857164+Blacktoothgrind@users.noreply.github.com>
2022-11-02 22:05:21 -0300

    Create operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,12 @@
+#Funciones de calculadora
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
